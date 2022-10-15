d589ff1a7672c791b1b04bff7a24f77c5e58b197
Pierce Yu <pierceyu0315@live.com>
2022-10-15 12:06:50 -0400

    progressing

--- background.js
+++ background.js
@@ -28,7 +28,7 @@ function myFunction() {
 }
 
 function startTimer(duration, display) {
-    let startTime = new Date()
+    // let startTime = new Date()
     let timer = duration;
     let interval = setInterval(function () {
     let minutes = parseInt(timer / 60, 10);
@@ -43,22 +43,24 @@ function startTimer(duration, display) {
             begin === 25 ? alert("time to take a break!"): alert("time to go back to work!");
             clearInterval(interval);
             display.innerHTML = '00:00';
-        } 
+        }
+        
         stopButton.addEventListener("click", ()=>{
-            let remainingTime = begin - (new Date() - startTime)
-            console.log(remainingTime)
-            if (!hasStarted) {
+            // let remainingTime = begin - (new Date() - startTime)
+            // console.log(remainingTime)
+            // if (!hasStarted) {
+            // clearInterval(interval)
+            // hasStarted = true
+            // }
+            // else if (hasStarted && needsToResmue) {
+            //     startTimer(remainingTime, display)
+            // }
             clearInterval(interval)
-            hasStarted = true
-            }
-            else if (hasStarted && needsToResmue) {
-                startTimer(remainingTime, display)
-            }
         })
 
 
 
-    }, 1);
+    }, 20);
     hasStarted = false
     needsToResmue = true
 }
